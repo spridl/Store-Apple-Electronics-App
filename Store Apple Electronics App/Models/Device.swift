07f1f56164c5c6eb57e216418f7ff84f83d61401
nasim <nasim_nozirov@mail.ru>
2022-03-31 11:57:09 +0300

    1

--- Store Apple Electronics App/Models/Device.swift
+++ Store Apple Electronics App/Models/Device.swift
@@ -2,7 +2,45 @@
 //  Device.swift
 //  Store Apple Electronics App
 //
-//  Created by Тимур on 29.03.2022.
+//   Created by Nasim Nozirov on 28.03.2022.
 //
 
 import Foundation
+
+
+struct Divice {
+    
+//    let iphone: String
+    let Price: Int
+    let descriptio: String
+    let Image: String
+    
+    
+    static func getStore() -> [Divice] {
+        
+        var divices: [Divice] = []
+        
+        let iphones = DataManager.shared.iPhone
+        let iphonesPrice = DataManager.shared.iphonePrice
+        let descriptioIphones = DataManager.shared.descriptionIPhone
+        let iphonesImage = DataManager.shared.iphoneImage
+        
+        let iterationCount = min(
+            iphones.count,
+            iphonesPrice.count,
+            descriptioIphones.count,
+            iphonesImage.count)
+        
+        for index in 0..<iterationCount {
+            let divice = Divice(
+//                 iphone: iphones[index],
+                Price: iphonesPrice[index],
+                descriptio: descriptioIphones[index],
+                Image: iphonesImage[index]
+            )
+            divices.append(divice)
+        }
+        
+        return divices
+    }
+}
